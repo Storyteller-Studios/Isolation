78b7f3a5396bed208dd8ea6fa00c166f2d353a15
Raspberry-Monster <lele2194512339@outlook.com>
2024-02-01 14:21:29 +0800

    [Win2D] 实现Win2D移植

---
+++ ShaderTest.UWP/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Timers;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace ShaderTest.UWP
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public PixelShaderEffect effect;
+        public CanvasBitmap bitmap;
+        public float time = 0;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Init();
+        }
+        public void Init()
+        {
+            Canvas.CreateResources += async (s, e) =>
+            {
+                StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Shaders/effect.bin"));
+                IBuffer buffer = await FileIO.ReadBufferAsync(file);
+                var bytes = buffer.ToArray();
+                effect = new PixelShaderEffect(bytes);
+            };
+            selectPicture.Click += async (s, e) =>
+            {
+                var filePicker = new FileOpenPicker();
+                filePicker.FileTypeFilter.Add(".png");
+                var file = await filePicker.PickSingleFileAsync();
+                bitmap = await CanvasBitmap.LoadAsync(Canvas.Device, await file.OpenAsync(FileAccessMode.Read));
+                effect.Source1 = bitmap;
+            };
+            Canvas.Update += (s, e) =>
+            {
+                if (effect == null || bitmap == null)
+                    return;
+                time = Convert.ToSingle(e.Timing.TotalTime.TotalSeconds);
+                s.Invalidate();
+            };
+            Canvas.Draw += (s, e) =>
+            {
+                if (effect == null || bitmap == null)
+                    return;
+                effect.Properties["_iTime"] = time;
+                e.DrawingSession.DrawImage(effect);
+                e.DrawingSession.Dispose();
+            };
+        }
+    }
+}
---
+++ ShaderTest.UWP/Shaders - 副本/effect.hlsli
@@ -0,0 +1,47 @@
+#define D2D_INPUT_COUNT 1
+#define D2D_INPUT0_COMPLEX
+#include "d2d1effecthelpers.hlsli"
+
+float _iTime = 0.5;
+float2 _iScale = float2(0.5,0.5);
+float _iPower = 3;
+
+float f_random(in float2 _st)
+{
+    return frac((sin(dot(_st.xy, float2(12.9898, 78.233002))) * 43758.547));
+}
+
+float f_noise(in float2 _st)
+{
+    float2 i = floor(_st);
+    float2 f = frac(_st);
+    float a = f_random(i);
+    float b = f_random((i + float2(1.0, 0.0)));
+    float c = f_random((i + float2(0.0, 1.0)));
+    float d = f_random((i + float2(1.0, 1.0)));
+    float2 u = ((f * f) * (3.0 - (2.0 * f)));
+    return ((lerp(a, b, u.x) + (((c - a) * u.y) * (1.0 - u.x))) + (((d - b) * u.x) * u.y));
+}
+
+float4 draw_image(in float2 uv)
+{
+    float speed = 0.1;
+    float power = _iPower;
+    float nXAmp = (((sin((_iTime * speed)) + 1.0) * 0.5) * power);
+    float nX = f_noise((uv * nXAmp));
+    float nYAmp = (((cos((_iTime * speed)) + 1.0) * 0.5) * power);
+    float nY = f_noise((uv * nYAmp));
+    float2 uv2 = float2(nX, nY);
+    
+    float2 mirroredUV = abs(frac(uv2 - 0.5) - 0.5) * 2;
+
+    return D2DSampleInput(0, mirroredUV);
+}
+
+D2D_PS_ENTRY(main)
+{
+    float2 uv = D2DGetInputCoordinate(0).xy;
+	float2 scale = float2(_iScale.x * 3, _iScale.y * 2.1781 + 0.0389);
+	if(scale.x < 0.1) scale.x = 0.1;
+	return draw_image(uv / scale);
+}

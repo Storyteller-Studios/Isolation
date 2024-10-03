dbec9da4786e53df5ad87fc6a6b211a791c420bc
Raspberry-Monster <lele2194512339@outlook.com>
2024-10-03 14:40:47 +0800

    [Enhancement] Support Disable Lightwave and add randomize

--- ShaderTest.UWP/Shaders/effect.hlsl
+++ ShaderTest.UWP/Shaders/effect.hlsl
@@ -10,7 +10,10 @@ float3 color3 = float3(0.910,0.510,0.8);
 float3 color4 = float3(0.350,0.71,0.953);
 float Width = 800;
 float Height = 800;
-
+bool EnableLightWave = true;
+float RandomValue1 = 0;
+float RandomValue2 = 0;
+float RandomValue3 = 0;
 float2x2 f_Rot(in float _a)
 {
     float _s = sin(_a);
@@ -81,22 +84,30 @@ D2D_PS_ENTRY(main)
     float3 layer1 = lerp(color1, color2, smoothstep_custom(-0.3, 0.2, mul(tuv, transpose(f_Rot(radians(-5.0)))).x));
     float3 layer2 = lerp(color3, color4, smoothstep_custom(-0.3, 0.2, mul(tuv, transpose(f_Rot(radians(-5.0)))).x));
     float3 finalComp = lerp(layer1, layer2, smoothstep_custom(0.5, -0.3, tuv.y));
-    float3 hsv = rgb2hsv(finalComp);
+    if(EnableLightWave == false)
+    {
+        return float4(finalComp, 1.0);
+    }
+    else
+    {
+        float3 hsv = rgb2hsv(finalComp);
 
-    float2 p = -1.0 + 1.5 * uv.xy / iResolution.xy;
-	float t = iTime / 5.;
-    
-	float x = p.x;
-	float y = p.y;
-    
-	float mov0 = x+y+cos(sin(t)*2.0)*100.+sin(x/100.)*1000.;
-	float mov1 = y / 0.3 + t;
-	float mov2 = x / 0.2;
-    
-    float c1 = sin(mov1+t)/2.+mov2/2.-mov1-mov2+t;
-    float c2 = cos(c1+sin(mov0/1000.+t)+sin(y/40.+t)+sin((x+y)/100.)*3.);
-	float c3 = abs(sin(c2+cos(mov1+mov2+c2)+cos(mov2)+sin(x/1000.)));
-    
-    float3 col = hsv2rgb(float3(range(abs(c2), hsv.x * 0.95, hsv.x), range(c3, hsv.y, hsv.y * 0.85), range(c3, hsv.z, hsv.z * 0.85)));
-    return float4(col, 1.0);
+        float2 p = -1.0 + 1.5 * uv.xy / iResolution.xy;
+        float t = iTime / 5.;
+
+        float x = p.x;
+        float y = p.y;
+
+        float mov0 = x+y+cos(sin(t)*2.0)*100.+sin(x/100.)*1000.;
+        float mov1 = y / 0.3 + t;
+        float mov2 = x / 0.2;
+
+        float c1 = sin(mov1+t + RandomValue1)/2.+mov2/2.-mov1-mov2+t;
+        float c2 = cos(c1+sin(mov0/1000.+t - RandomValue2)+sin(y/40.+t + RandomValue3)+sin((x+y)/100.)*3.);
+        float c3 = abs(sin(c2+cos(mov1+mov2+c2)+cos(mov2)+sin(x/1000.)));
+
+        float3 col = hsv2rgb(float3(range(abs(c2), hsv.x * 0.95, hsv.x), range(c3, hsv.y, hsv.y * 0.85), range(c3, hsv.z, hsv.z * 0.85)));
+        return float4(col, 1.0);    
+    }
+
 }
